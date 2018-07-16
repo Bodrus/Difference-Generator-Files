f9f7c2a0bbacbca944d39227e83b57fbab55e65a
bodrus <maxshop2010@gmail.com>
2018-07-16 14:57:19 +0300

    do not works version, problem with import/export

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+// let commander = require('commander');
+// import commander from 'commander';
+import sum from './index';
 
-console.log('Welcome to calculator');
+console.log('jjjjj');
+
+console.log(sum(2, 2));
