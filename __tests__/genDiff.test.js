2f0c545d18067111c12b756b2ec2b07bd419545a
bodrus <maxshop2010@gmail.com>
2018-07-20 02:50:18 +0300

    add function getAst

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,26 +1,49 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const pathToExpected = '__tests__/__fixtures__/expected.txt';
-const pathJsonBefore = '__tests__/__fixtures__/before.json';
-const pathJsonAfter = '__tests__/__fixtures__/after.json';
-const pathYamlBefore = '__tests__/__fixtures__/before.yml';
-const pathYamlAfter = '__tests__/__fixtures__/after.yml';
-const pathIniBefore = '__tests__/__fixtures__/before.ini';
-const pathIniAfter = '__tests__/__fixtures__/after.ini';
+// const pathToExpectedTreeTest = '__tests__/__fixtures__/__testFixtures__/testTest.txt';
+// const pathJsonBeforeTreeTest = '__tests__/__fixtures__/__testFixtures__/afterTest.json';
+// const pathJsonAfterTreeTest = '__tests__/__fixtures__/__testFixtures__/beforeTest.json';
+
+const pathToExpectedTree = '__tests__/__fixtures__/treeExpected.txt';
+const pathJsonBeforeTree = '__tests__/__fixtures__/beforeTree.json';
+const pathJsonAfterTree = '__tests__/__fixtures__/afterTree.json';
+
+// const pathYamlBeforeTree = '__tests__/__fixtures__/before.yml';
+// const pathYamlAfterTree = '__tests__/__fixtures__/after.yml';
+// const pathIniBeforeTree = '__tests__/__fixtures__/before.ini';
+// const pathIniAfterTree = '__tests__/__fixtures__/after.ini';
+
+// const pathToExpected = '__tests__/__fixtures__/expected.txt';
+// const pathJsonBefore = '__tests__/__fixtures__/before.json';
+// const pathJsonAfter = '__tests__/__fixtures__/after.json';
+// const pathYamlBefore = '__tests__/__fixtures__/before.yml';
+// const pathYamlAfter = '__tests__/__fixtures__/after.yml';
+// const pathIniBefore = '__tests__/__fixtures__/before.ini';
+// const pathIniAfter = '__tests__/__fixtures__/after.ini';
 
 
-test('json', () => {
-  const actual = genDiff(pathJsonBefore, pathJsonAfter);
-  expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
+// test('json', () => {
+//   const actual = genDiff(pathJsonBefore, pathJsonAfter);
+//   expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
+// });
+
+// test('yaml', () => {
+//   const actual = genDiff(pathYamlBefore, pathYamlAfter);
+//   expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
+// });
+
+// test('ini', () => {
+//   const actual = genDiff(pathIniBefore, pathIniAfter);
+//   expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
+// });
+
+test('jsonTree', () => {
+  const actual = genDiff(pathJsonBeforeTree, pathJsonAfterTree);
+  expect(actual).toBe(fs.readFileSync(pathToExpectedTree, 'utf8'));
 });
 
-test('yaml', () => {
-  const actual = genDiff(pathYamlBefore, pathYamlAfter);
-  expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
-});
-
-test('ini', () => {
-  const actual = genDiff(pathIniBefore, pathIniAfter);
-  expect(actual).toBe(fs.readFileSync(pathToExpected, 'utf8'));
-});
+// test('jsonTree', () => {
+//   const actual = genDiff(pathJsonBeforeTreeTest, pathJsonAfterTreeTest);
+//   expect(actual).toBe(fs.readFileSync(pathToExpectedTreeTest, 'utf8'));
+// });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import yaml from 'js-yaml';
 import path from 'path';
 import ini from 'ini';
 
+
 const parsers = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
@@ -16,35 +17,65 @@ const getData = (pathFile) => {
   const extName = path.extname(pathFile);
   const parser = getParser(extName);
   const data = fs.readFileSync(pathFile, 'utf-8');
-  return parser(data);
+  const result = parser(data);
+  return result;
 };
-
 
 const getKeysFromObjects = (after, before) => _.union(_.keys(after), _.keys(before));
 
-
-const getConvertTostring = (before, after) => {
+const getAst = (before, after) => {
   const keys = getKeysFromObjects(after, before);
 
-  const newString = keys.reduce((acc, key) => {
+  return keys.reduce((acc, key) => {
     if (_.has(after, key) && !_.has(before, key)) {
-      return [...acc, `+ ${key}: ${after[key]}`];
-    } if (_.has(after, key)) {
-      if (before[key] === after[key]) {
-        return [...acc, ` ${key}: ${after[key]}`];
-      }
-      const beforeStr = `- ${key}: ${before[key]}`;
-      const afterStr = `+ ${key}: ${after[key]}`;
-      return [...acc, `${beforeStr}`, `${afterStr}`];
+      return [...acc, {
+        type: 'added', key, beforeValue: '', afterValue: after[key], children: [],
+      }];
     }
-    return [...acc, `- ${key}: ${before[key]}`];
+    if (_.has(after, key) && _.has(before, key)) {
+      if (before[key] instanceof Object && after[key] instanceof Object) {
+        return [...acc, {
+          type: 'unchanged', key, beforeValue: '', afterValue: '', children: getAst(before[key], after[key]),
+        }];
+      }
+      if (before[key] === after[key]) {
+        return [...acc, {
+          type: 'uncheged', key, beforeValue: before[key], afterValue: after[key], children: [],
+        }];
+      }
+      return [...acc, {
+        type: 'changed', key, beforeValue: before[key], afterValue: after[key], children: [],
+      }];
+    }
+    return [...acc, {
+      type: 'deleted', key, beforeValue: before[key], afterValue: '', children: [],
+    }];
   }, []);
-
-  return newString.join('\n');
 };
+
+// const getConvertTostring = (before, after) => {
+  //   const keys = getKeysFromObjects(after, before);
+
+//   const newString = keys.reduce((acc, key) => {
+  //     if (_.has(after, key) && !_.has(before, key)) {
+//       return [...acc, `+ ${key}: ${after[key]}`];
+//     } if (_.has(after, key)) {
+//       if (before[key] === after[key]) {
+  //         return [...acc, ` ${key}: ${after[key]}`];
+  //       }
+  //       const beforeStr = `- ${key}: ${before[key]}`;
+  //       const afterStr = `+ ${key}: ${after[key]}`;
+//       return [...acc, `${beforeStr}`, `${afterStr}`];
+//     }
+//     return [...acc, `- ${key}: ${before[key]}`];
+//   }, []);
+
+//   return newString.join('\n');
+// };
 
 
 export default (a, b) => {
-  const result = getConvertTostring(getData(a), getData(b));
+  const result = getAst(getData(a), getData(b));
+  // const result = getConvertTostring(getData(a), getData(b));
   return result;
 };
